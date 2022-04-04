86802619ab5026d7955a6257c99b28b0de92d9e9
Edgar Carrillo <ecarrillo046@gmail.com>
2022-04-04 12:07:34 -0700

    feat: add folders and files for each member

--- react-client/src/components/App.jsx
+++ react-client/src/components/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Overview from './Overview/Overview.jsx';
+import QAndAs from './QuestionsAndAnswers/QAndAs.jsx';
+import RatingsAndReviews from './RatingsAndReviews/RatingsAndReviews.jsx';
+import Suggestions from './Suggestions/Suggestions.jsx';
 
 function App() {
   return (
     <div>
-      This is the App Component. Add components in the components sub-directory.
+      <Overview />
+      <QAndAs />
+      <RatingsAndReviews />
+      <Suggestions />
     </div>
   );
 }
---
+++ react-client/src/components/QuestionsAndAnswers/QAndAs.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function QAndAs() {
+  return <div />;
+}
+
+export default QAndAs;
