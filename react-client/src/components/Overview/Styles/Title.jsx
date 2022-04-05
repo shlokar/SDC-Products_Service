02265d2fdbff2a3ae48b926739c88016c3cb4393
Edgar Carrillo <ecarrillo046@gmail.com>
2022-04-05 12:15:36 -0700

    feat: add Title for Style selection in overview

---
+++ react-client/src/components/Overview/Styles/Title.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import propTypes from 'prop-types';
+
+const BoldText = styled.span`
+  font-family: var(--fnt-dark);
+`;
+
+function TitleContainer({ className, styleName }) {
+  return (
+    <div className={className}>
+      <p>
+        <BoldText>Style &gt;</BoldText>
+        {' '}
+        {styleName}
+      </p>
+    </div>
+  );
+}
+
+const StyledTitleContainer = styled(TitleContainer)`
+  display: flex;
+  font-size: 1.3rem;
+  font-family: var(--fnt-light);
+  text-transform: uppercase;
+`;
+
+TitleContainer.propTypes = {
+  className: propTypes.string.isRequired,
+  styleName: propTypes.string.isRequired,
+};
+
+export default StyledTitleContainer;
