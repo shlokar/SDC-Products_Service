211535117f323058eaebc8f44a4fdb55ae63651c
Edgar Carrillo <ecarrillo046@gmail.com>
2022-04-06 21:54:49 -0700

    feat: add AddToCartContainer component

---
+++ react-client/src/components/Overview/AddToCart/AddToCartContainer.jsx
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import propTypes from 'prop-types';
+
+// Components
+import StyledSelectSize from './SelectSize.jsx';
+import QtySelector from './QtySelector.jsx';
+import StyledAddToCartBtn from './AddToCartBtn.jsx';
+import StyledAlert from './Alert.jsx';
+
+function AddToCartContainer({ className, items }) {
+  const [selectedItem, setSelectedItem] = useState({ stock: 0 });
+  const [userSelection, setUserSelection] = useState({ size: '', qty: '0' });
+  const [alertVisible, setAlertVisible] = useState(false);
+  const hasStylesInStock = items.some((item) => item.stock > 0);
+
+  const updateSelectedItem = (id) => {
+    if (selectedItem.id !== id) {
+      for (let i = 0; i < items.length; i++) {
+        if (items[i].id === id) {
+          setSelectedItem(items[i]);
+          break;
+        }
+      }
+    }
+  };
+
+  const changeUserSize = (size) => {
+    const newUserSelection = { ...userSelection };
+    newUserSelection.size = size;
+    newUserSelection.qty = String(1);
+    setUserSelection(newUserSelection);
+  };
+
+  const changeUserQty = (qty) => {
+    const newUserSelection = { ...userSelection };
+    newUserSelection.qty = qty;
+    setUserSelection(newUserSelection);
+  };
+
+  const getSize = (id) => {
+    const { value } = items.filter((item) => item.id === id)[0];
+    return value;
+  };
+
+  const showAlert = () => {
+    setAlertVisible(true);
+  };
+
+  const hideAlert = () => {
+    setAlertVisible(false);
+  };
+
+  return (
+    <div>
+      <StyledAlert text="Please select size." isVisible={alertVisible} />
+      <div className={className}>
+        <StyledSelectSize
+          selections={items}
+          selectHandler={(e) => {
+            if (alertVisible) {
+              hideAlert(false);
+            }
+            changeUserSize(getSize(e.target.value));
+            updateSelectedItem(e.target.value);
+          }}
+        />
+        <QtySelector
+          qty={selectedItem.stock}
+          clickHandler={changeUserQty}
+          userQty={userSelection.qty}
+        />
+        <StyledAddToCartBtn
+          hasStylesInStock={hasStylesInStock}
+          actionHandler={() => {
+            const { size, qty } = userSelection;
+
+            if (size.length > 0 && Number(qty) > 0) {
+              console.log('Ready to submit! Here is the data: ', userSelection);
+            } else {
+              showAlert();
+            }
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+AddToCartContainer.propTypes = {
+  className: propTypes.string.isRequired,
+  items: propTypes.arrayOf(propTypes.shape({
+    id: propTypes.string.isRequired,
+    value: propTypes.string.isRequired, // The size of the product (large, medium, etc.)
+    stock: propTypes.number.isRequired, // the amount of the product in stock.
+  })).isRequired,
+};
+
+const StyledAddToCartContainer = styled(AddToCartContainer)`
+  display: flex;
+  justify-content: space-between;
+  gap: 30px;
+  flex-wrap: wrap;
+  max-width: 450px;
+`;
+
+export default StyledAddToCartContainer;
