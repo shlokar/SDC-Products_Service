4096fadc3f18d79243a375c138767c2f67bb0208
Edgar Carrillo <ecarrillo046@gmail.com>
2022-04-08 13:44:18 -0700

    feat: create left-arrow component

--- react-client/src/components/Overview/ImageGallery/Arrows.jsx
+++ react-client/src/components/Overview/ImageGallery/Arrows.jsx
@@ -5,6 +5,7 @@ import propTypes from 'prop-types';
 // Assets
 import upArrowSrc from './up-arrow-icon.svg';
 import downArrowSrc from './down-arrow-icon.svg';
+import leftArrowSrc from './left-arrow-icon.svg';
 
 function UpArrow({ className, clickHandler }) {
   return (
@@ -40,4 +41,45 @@ DownArrow.propTypes = {
   clickHandler: propTypes.func.isRequired,
 };
 
-export { StyledUpArrow, StyledDownArrow };
+const ArrowBody = styled.div`
+  position: absolute;
+  height: 4px;
+  background-color: black;
+  width: 15px;
+  left: 8px;
+`;
+
+const ArrowContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  height: 20px;
+  width: 20px;
+  cursor: pointer;
+`;
+
+function LeftArrow({ className, clickHandler }) {
+  return (
+    <ArrowContainer onClick={() => console.log('asdf')}>
+      <input type="image" src={leftArrowSrc} alt="scroll-down-button" className={className} />
+      <ArrowBody />
+    </ArrowContainer>
+  );
+}
+
+const StyledLeftArrow = styled(LeftArrow)`
+  position: absolute;
+  cursor: pointer;
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+  ${({ visible }) => !visible && 'visibility: hidden;'}
+`;
+
+LeftArrow.propTypes = {
+  className: propTypes.string.isRequired,
+  clickHandler: propTypes.func.isRequired,
+};
+
+export { StyledUpArrow, StyledDownArrow, StyledLeftArrow };
