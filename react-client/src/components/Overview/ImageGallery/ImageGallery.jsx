6fc5207698127410c212071877f2875518be5799
Edgar Carrillo <ecarrillo046@gmail.com>
2022-04-08 13:26:25 -0700

    feat: add main image fading animation on change

--- react-client/src/components/Overview/ImageGallery/ImageGallery.jsx
+++ react-client/src/components/Overview/ImageGallery/ImageGallery.jsx
@@ -3,10 +3,14 @@ import styled from 'styled-components';
 import propTypes from 'prop-types';
 import uniqid from 'uniqid';
 
+// Assets
+import useTracker from './ThumbnailContent/useTracker';
+
 // Components
 import StyledThumbnailsContainer from './ThumbnailContent/ThumbnailsContainer.jsx';
 import StyledMainImage from './MainImage.jsx';
 import StyledExpandedImage from './ExpandedImage.jsx';
+import StyledAnimateImg from './AnimateImg.jsx';
 
 const StyledContainer = styled.div`
   display: flex;
@@ -21,6 +25,7 @@ const LeftDiv = styled.div`
 const RightDiv = styled.div`
   position: relative;
   width: 100%;
+  overflow: hidden;
 `;
 
 const createCustomImgsArr = (arr) => {
@@ -31,11 +36,11 @@ const createCustomImgsArr = (arr) => {
     return newObj;
   });
 
-  return arrCopy;
+  return useTracker(arrCopy).arr;
 };
 
 function ImageGallery({ className, data }) {
-  const [customImgsArr, setCustomImgsArr] = useState(createCustomImgsArr(data));
+  const [customImgsArr] = useState(createCustomImgsArr(data));
   const [currImg, setCurrImg] = useState(customImgsArr[0]);
   const [expandedViewVisible, setExpandedViewVisible] = useState(false);
 
@@ -50,7 +55,15 @@ function ImageGallery({ className, data }) {
           />
         </LeftDiv>
         <RightDiv>
-          <StyledMainImage src={currImg.url} alt="#" clickHandler={() => setExpandedViewVisible(true)} />
+          {customImgsArr.map((image) => (
+            <StyledAnimateImg key={image.id} selected={currImg.id === image.id}>
+              <StyledMainImage
+                src={image.url}
+                alt="#"
+                clickHandler={() => setExpandedViewVisible(true)}
+              />
+            </StyledAnimateImg>
+          ))}
         </RightDiv>
       </StyledContainer>
     </div>
@@ -62,7 +75,7 @@ const StyledImageGallery = styled(ImageGallery)`
   box-sizing: border-box;
   height: 800px;
   width: 1000px;
-  border: 2px solid black;
+  background-color: #EDEFF0;
   padding: 20px;
 `;
 
