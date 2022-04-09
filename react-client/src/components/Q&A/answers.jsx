dae4b0ca492cc981e447a237fe307f838da7bda5
Chris <chris.padovan5@gmail.com>
2022-04-09 13:44:46 -0700

    feat/Q\&A updates to answers component

--- react-client/src/components/Q&A/answers.jsx
+++ react-client/src/components/Q&A/answers.jsx
@@ -3,43 +3,50 @@ import moment from 'moment';
 import { useState } from 'react';
 
 export default function Answers({ answer }) {
-  let loaded = Object.keys(answer).map((element) => {
-    return (
-      <div>
-        <h2>
-          A:
-        </h2>
-        <p>{answer[element].body}</p>
-        <div>
-          by {answer[element].answerer_name}
-          - Seller,
-          {moment(answer[element].question_date).format('MMMM, D, YYYY')}
-          |
-        </div>
-        <div>
-          Helpful? Yes: (
-          {answer[element].helpfulness}
-          ) | Report
-        </div>
-      </div>
-    )
-  });
+  const [answerIsClicked, setClicked] = useState(false);
+
+  const handleIncrement = (element) => {
+    console.log('this ran', answer[element].helpfulness, answerIsClicked)
+    if (answerIsClicked === false) {
+      answer[element].helpfulness += 1;
+      setClicked(true);
+    }
+  };
+  const loaded = Object.keys(answer).map((element) =>
+    <div>
+      <h2>
+        A:
+      </h2>
+      <p>{answer[element].body}</p>
+      <span>
+        by {answer[element].answerer_name}
+        - Seller,
+        {moment(answer[element].question_date).format('MMMM, D, YYYY')}
+        |
+      </span>
+      <span>
+        Helpful?
+        </span>
+        <span onClick={() => handleIncrement(element)}> Yes:</span>
+        <span> (
+        {answer[element].helpfulness}
+        ) | Report
+      </span>
+    </div>);
 
   const [current, setCurrent] = useState([loaded[0], loaded[1]]);
 
-  let handleClick = () => {
+  const handleClick = () => {
     setCurrent(loaded);
   };
   return (
     <div>
-  <div>
-    {current.map((element) => {
-      return element;
-    })}
-  </div>
-  <div>
-    <input type="button" value="More Answers" onClick={handleClick}></input>
-  </div>
-  </div>
+      <div>
+        {current.map((element) => element)}
+      </div>
+      <div>
+        <input type="button" value="More Answers" onClick={handleClick}></input>
+      </div>
+    </div>
   )
 }
