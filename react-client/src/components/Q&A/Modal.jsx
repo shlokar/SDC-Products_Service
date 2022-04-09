1240963cadd96b0d4a911ce7683fa2e6c00a47d8
Chris <chris.padovan5@gmail.com>
2022-04-09 15:48:40 -0700

    Modal progress without styling

---
+++ react-client/src/components/Q&A/Modal.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// components
+import NewQuestion from './newQuestion.jsx';
+
+const ModalWrapper = styled.div`
+  width: 800px;
+  height: 500px;
+  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+  background: #fff;
+  color: #000;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  position: relative;
+  z-index: 10:
+  border-radius: 10px;
+`;
+
+const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align items: center;
+`
+
+export default function Modal({ showModal, setShowModal, addQuestion, setAddedQuestion }) {
+  return (
+    <>
+        {/* {showModal ? (
+          <Background>
+            <ModalWrapper showModal={showModal}></ModalWrapper>
+            <h1>hi</h1>>
+          </Background>
+        ): null} */}
+        {showModal ? <NewQuestion addQuestion={addQuestion} setAddedQuestion={setAddedQuestion}/> : null}
+    </>
+  )
+}
