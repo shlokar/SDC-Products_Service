2440e5e56b498f5130c17d261213a51e0242daca
Chris <chris.padovan5@gmail.com>
2022-04-11 12:30:37 -0700

    feat/q: styled questions component

--- react-client/src/components/Q&A/questions.jsx
+++ react-client/src/components/Q&A/questions.jsx
@@ -1,37 +1,80 @@
 import React from 'react';
 import { useState } from 'react';
+import styled from 'styled-components';
 
 // components
 import Answers from './answers.jsx';
+import AnswerModal from './AnswerModal.jsx'
+
+const AnswerButton = styled.span`
+  text-decoration: underline;
+  font-family: 'Roboto-Thin';
+  font-size 11px;
+  color: #696969;
+  cursor: pointer;
+`;
+
+const QuestionHeader = styled.span`
+  font-weight: bold;
+  position: relative
+  padding-top: 10px;
+  padding-left: 4px;
+`;
+
+const QuestionFunctions = styled.span`
+  font-size 11px;
+  color: #696969;
+  padding: 1px;
+  cursor: pointer;
+`;
+
+const QuestionFunctionsWrapper = styled.div`
+  position: relative;
+  top: -18px;
+  left: 70%;
+`;
 
 export default function Questions({ question }) {
   const [isClicked, setClicked] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   const handleIncrement = () => {
     if (isClicked === false) {
       question.question_helpfulness += 1;
       setClicked(true);
     }
   }
+
+  const openModal = () => {
+    setShowModal((prev) => !prev);
+  };
+
   return (
     <span>
-      <h2>
+      <QuestionHeader>
         Q:
-      </h2>
-      <span>
-        Helpful?
-         </span>
-        <span onClick={handleIncrement}>
-          Yes
-        </span>
-        <span>
-         (
-        {question.question_helpfulness}
-        ) | Add Answer
-      </span>
-      <div>
+      </QuestionHeader>
+      <QuestionHeader>
         {question.question_body}
-      </div>
-      <Answers answer={question.answers}/>
+      </QuestionHeader>
+      <QuestionFunctionsWrapper>
+        <QuestionFunctions>
+          Helpful?&nbsp;
+        </QuestionFunctions>
+        <QuestionFunctions style={{ textDecorationLine: 'underline' }} onClick={handleIncrement}>
+          Yes&nbsp;
+        </QuestionFunctions>
+        <QuestionFunctions>
+          (
+          {question.question_helpfulness}
+          ) &nbsp;|&nbsp;
+        </QuestionFunctions>
+        <AnswerButton onClick={openModal}>Add Answer</AnswerButton>
+      </QuestionFunctionsWrapper>
+      <AnswerModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+      />
+      <Answers answer={question.answers} />
     </span>
   );
 }
