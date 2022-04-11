6a896f8fcbd13427d2fb1a345b42c570fa00511f
Chris <chris.padovan5@gmail.com>
2022-04-11 12:26:05 -0700

    feat/q: added answerModal

---
+++ react-client/src/components/Q&A/AnswerModal.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// components
+import NewAnswer from './NewAnswer.jsx';
+
+const ModalWrapper = styled.div`
+  width: 800px;
+  height: 500px;
+  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+  background: #fff;
+  color: #000;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  position: absolute;
+  z-index: 10:
+  border-radius: 10px;
+`;
+
+const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align items: center;
+`;
+
+export default function AnswerModal({ showModal, setShowModal }) {
+  return (
+    <>
+      {showModal ? <NewAnswer /> : null}
+    </>
+  );
+}
