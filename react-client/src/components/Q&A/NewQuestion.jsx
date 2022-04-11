85fbb2411d1c1b00cdf4a8f7924d685c2ed2c720
Chris <chris.padovan5@gmail.com>
2022-04-11 12:28:35 -0700

    feat/q added in question modal form

--- react-client/src/components/Q&A/NewQuestion.jsx
+++ react-client/src/components/Q&A/NewQuestion.jsx
@@ -1,5 +1,44 @@
 import React, { useState } from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
+
+const ModalWrapper = styled.div`
+position: absolute;
+position-left: 25%;
+border: 3px solid #000;
+background-color: #fff;
+`;
+
+const StyledTextInput = styled.input`
+font-weight: bold;
+border-style: solid;
+border-width: 1px;
+border-color: black;
+width: 95%;
+height: 20px;
+padding-left: 15px;
+background-repeat: no-repeat;
+`;
+
+const StyledBodyInput = styled.input`
+font-weight: bold;
+border-style: solid;
+border-width: 1px;
+border-color: black;
+width: 95%;
+height: 20px;
+padding-left: 15px;
+`;
+
+const StyledButton = styled.button`
+// min-width: 100px;
+padding: 8px 16px;
+// border-radius: 1px;
+border: solid;
+background: #fff;
+color: #141414;
+font-size: 16px;
+cursor: pointer;
+`;
 
 export default function NewQuestion({ addQuestion, setAddedQuestion }) {
   const [body, setBody] = useState('');
@@ -29,17 +68,18 @@ export default function NewQuestion({ addQuestion, setAddedQuestion }) {
   };
 
   return (
-    <div>
+    <ModalWrapper>
       <h2>Ask Your Question</h2>
       <h4>About the (product name here)</h4>
       <form onSubmit={handleSubmit}>
-        <input type="textarea" placeholder="Why did you like the product or not?" value={body} onChange={(e) => setBody(e.target.value)} />
-        <input type="textarea" placeholder="Example: jackson11!" value={name} onChange={(e) => setName(e.target.value)} />
+        <StyledBodyInput type="textarea" placeholder="Why did you like the product or not?" value={body} onChange={(e) => setBody(e.target.value)} />
+        <div>&nbsp;</div>
+        <StyledTextInput type="textarea" placeholder="Example: jackson11!" value={name} onChange={(e) => setName(e.target.value)} />
         <p>For privacy reasons, do not use your full name or email address</p>
-        <input type="textarea" placeholder="Your email" value={email} onChange={(e) => setEmail(e.target.email)} />
+        <StyledTextInput type="textarea" placeholder="Your email" value={email} onChange={(e) => setEmail(e.target.email)} />
         <p>For authentication reasons, you will not be emailed.</p>
-        <input type="submit" value="Submit Question" />
+        <StyledButton type="submit" value="Submit Question">Submit</StyledButton>
       </form>
-    </div>
+    </ModalWrapper>
   );
 }
