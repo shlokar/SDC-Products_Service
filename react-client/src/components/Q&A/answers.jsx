47dc55404e19054b4ae1e14d12683f3b8bca0068
Chris <chris.padovan5@gmail.com>
2022-04-11 12:29:45 -0700

    feat/q: styled answers component

--- react-client/src/components/Q&A/answers.jsx
+++ react-client/src/components/Q&A/answers.jsx
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
-import { useState } from 'react';
+import styled from 'styled-components';
+
+const Button = styled.span`
+  min-width: 50px;
+  padding: 8px 22px ;
+  border-radius: 4px;
+  background: #fff;
+  color: #141414;
+  font-size: 10px;
+  cursor: pointer;
+  font-weight: bold;
+`;
+
+const AnswerFunctions = styled.span`
+  font-size: 11px;
+  color: #696969;
+  padding-top: 10px;
+  padding-left: 2px;
+  line-height: 30px;
+  position: relative
+`;
+
+const AnswerFunctionsWrapper = styled.div`
+  padding-left: 22px;
+`;
+
+const AnswerA = styled.span`
+  font-weight: bold;
+  position: relative
+  padding-top: 10px;
+  padding-left: 4px;
+`;
+
+const StyledAnswerBody = styled.span`
+  position: relative
+  padding-top: 10px;
+  padding-left: 4px;
+  font-size: 13px;
+  color: #595959
+`;
 
 export default function Answers({ answer }) {
   const [answerIsClicked, setClicked] = useState(false);
 
   const handleIncrement = (element) => {
-    console.log('this ran', answer[element].helpfulness, answerIsClicked)
+    console.log('this ran', answer[element].helpfulness, answerIsClicked);
     if (answerIsClicked === false) {
       answer[element].helpfulness += 1;
       setClicked(true);
@@ -14,24 +53,30 @@ export default function Answers({ answer }) {
   };
   const loaded = Object.keys(answer).map((element) =>
     <div>
-      <h2>
+      <AnswerA>
         A:
-      </h2>
-      <p>{answer[element].body}</p>
-      <span>
-        by {answer[element].answerer_name}
-        - Seller,
-        {moment(answer[element].question_date).format('MMMM, D, YYYY')}
-        |
-      </span>
-      <span>
-        Helpful?
-        </span>
-        <span onClick={() => handleIncrement(element)}> Yes:</span>
-        <span> (
-        {answer[element].helpfulness}
-        ) | Report
-      </span>
+      </AnswerA>
+      <StyledAnswerBody>{answer[element].body}</StyledAnswerBody>
+      <div>
+        <AnswerFunctionsWrapper>
+        <AnswerFunctions>
+          by&nbsp;
+          {answer[element].answerer_name} &nbsp;|
+          &nbsp;
+          {moment(answer[element].question_date).format('MMMM, D, YYYY')}
+          &nbsp; | &nbsp;
+        </AnswerFunctions>
+        <AnswerFunctions>
+          Helpful?
+        </AnswerFunctions>
+        <AnswerFunctions style={{ textDecorationLine: 'underline' }}
+        onClick={() => handleIncrement(element)}>Yes</AnswerFunctions>
+        <AnswerFunctions> (
+          {answer[element].helpfulness}
+          ) | Report
+        </AnswerFunctions>
+        </AnswerFunctionsWrapper>
+      </div>
     </div>);
 
   const [current, setCurrent] = useState([loaded[0], loaded[1]]);
@@ -45,8 +90,9 @@ export default function Answers({ answer }) {
         {current.map((element) => element)}
       </div>
       <div>
-        <input type="button" value="More Answers" onClick={handleClick}></input>
+        <Button onClick={handleClick}>LOAD MORE ANSWERS</Button>
       </div>
+      <div>&nbsp;</div>
     </div>
-  )
+  );
 }
