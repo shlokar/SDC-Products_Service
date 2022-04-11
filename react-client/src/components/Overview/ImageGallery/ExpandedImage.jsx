c3e6e83590e994c4cc43b89182cee89d9ca13f3d
Edgar Carrillo <ecarrillo046@gmail.com>
2022-04-10 17:29:35 -0700

    feat: add zoom in mode

--- react-client/src/components/Overview/ImageGallery/ExpandedImage.jsx
+++ react-client/src/components/Overview/ImageGallery/ExpandedImage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, createRef } from 'react';
 import styled from 'styled-components';
 import propTypes from 'prop-types';
 
@@ -6,23 +6,66 @@ const StyledInput = styled.input`
   width: 100%;
   object-fit: cover;
   cursor: zoom-in;
+  ${({ isScaled }) => isScaled && 'cursor: crosshair;'}
+  ${({ isScaled }) => isScaled && 'transform: scale(3);'}
+  transform-origin: ${({ isScaled, x, y }) => isScaled && `${x}% ${y}%`};
+  transition: 1s all;
 `;
 
 const StyledContainer = styled.div`
   display: flex;
   position: absolute;
   width: 60%;
+  ${({ isScaled }) => isScaled && 'width: 70%;'}
   box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;
   overflow: hidden;
+  transition: all 1s;
 `;
 
 function ExpandedImage({
   className, src, alt, clickHandler,
 }) {
+  const [isScaled, setIsScaled] = useState(false);
+  const [xAxis, setXAxis] = useState(0);
+  const [yAxis, setYAxis] = useState(0);
+  const node = createRef();
+
   return (
     <div className={className}>
-      <StyledContainer>
-        <StyledInput type="image" src={src} alt={alt} onClick={() => clickHandler()} />
+      <StyledContainer
+        ref={node}
+        isScaled={isScaled}
+        xAxis={xAxis}
+        yAxis={yAxis}
+        onMouseMove={(e) => {
+          if (isScaled) {
+            const data = node.current.getBoundingClientRect();
+
+            const offsetX = e.nativeEvent.layerX;
+            const offsetY = e.nativeEvent.layerY;
+            const xPercent = Math.ceil(((offsetX / data.width) * 100));
+            const yPercent = Math.ceil((offsetY / data.height) * 100);
+            setXAxis(xPercent);
+            setYAxis(yPercent);
+          }
+        }}
+      >
+        <StyledInput
+          isScaled={isScaled}
+          type="image"
+          x={xAxis}
+          y={yAxis}
+          src={src}
+          alt={alt}
+          onClick={() => {
+            if (!isScaled) {
+              setIsScaled(true);
+            } else {
+              setIsScaled(false);
+              clickHandler();
+            }
+          }}
+        />
       </StyledContainer>
     </div>
   );
