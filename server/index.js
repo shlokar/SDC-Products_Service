78fc0a81e627aced68a654473c9c72f52681ed42
Edgar Carrillo <ecarrillo046@gmail.com>
2022-04-14 11:10:44 -0700

    feat: incorporate cors

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const path = require('path');
+const cors = require('cors');
 const express = require('express');
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
